ebe5e635f6d6bbf7efbcb1ab6e02d45cec77d1d9
J1stHum2n <60404713+J1stHum2n@users.noreply.github.com>
2022-05-23 17:43:07 +0300

    Add files via upload

---
+++ Birthday-B-D/Birthday-B-D/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Birthday-B-D
+//
+//  Created by Student on 23.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var Add: UIButton!
+    @IBOutlet weak var Delete: UIButton!
+    
+    var table = [String]
+    
+    @IBAction func AddTApped(_ sender: UIButton) {
+        table.append()
+    }
+    
+    
+}
